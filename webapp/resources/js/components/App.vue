04d72b6ce57c8f3538a44c2aef31a633aa2f5242
kita127 <kitada.kosuke@gmail.com>
2023-08-15 18:42:02 +0900

    up: フロント実装

--- webapp/resources/js/components/App.vue
+++ webapp/resources/js/components/App.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import axios from "axios";
 
 const name = ref("田中太郎");
+
+const getData = async () => {
+    const url = "api/entries";
+    let response = await axios.get(url);
+    return response;
+};
+
+const entriesData: { entryId: string, title: string }[] = [];
+const entries = ref(entriesData);
+
+getData().then(result => { entries.value = result.data.entries });
+
+
 </script>
 
 <template>
     <h1>こんにちは!{{ name }}さん!</h1>
+    <div v-for="(entry, index) in entries">
+        {{ entry.entryId }} : {{ entry.title }}
+    </div>
 </template>
