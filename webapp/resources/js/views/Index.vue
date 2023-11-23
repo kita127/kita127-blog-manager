2e24df54afa5753756127f00b8112ae1235f7374
kita127 <kitada.kosuke@gmail.com>
2023-11-23 12:21:58 +0900

    mod: RouteLinkを使用してコンポーネント名でリンク

--- webapp/resources/js/views/Index.vue
+++ webapp/resources/js/views/Index.vue
@@ -1,12 +1,13 @@
 <template>
     <h1>Index</h1>
-    <div>
-        <router-link to="/about">About</router-link>
-    </div>
     <div> {{ message }} </div>
+    <p>
+        <RouterLink v-bind:to="{ name: 'About' }">About</RouterLink>
+    </p>
 </template>
 <script lang="ts" setup>
 import { ref } from "vue";
+import { RouterLink } from "vue-router";
 const message = ref<string>('index message');
 </script>
   
