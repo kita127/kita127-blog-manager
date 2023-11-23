1cdf46e557d227ad917dca4048718e1db5600c2e
kita127 <kitada.kosuke@gmail.com>
2023-11-23 14:45:55 +0900

    update: トップページ作成

--- webapp/resources/js/views/Index.vue
+++ webapp/resources/js/views/Index.vue
@@ -1,13 +1,55 @@
 <template>
-    <h1>Index</h1>
-    <div> {{ message }} </div>
+    <h1>ブログマネージャーサービス</h1>
+
+    <section>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>タイトル</th>
+                    <th>詳細</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="[id, article] in articles">
+                    <td>{{ article.id }}</td>
+                    <td>{{ article.title }}</td>
+                    <td>詳細内容</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+
+    <!--
     <p>
         <RouterLink v-bind:to="{ name: 'About' }">About</RouterLink>
     </p>
+-->
 </template>
+
 <script lang="ts" setup>
 import { ref } from "vue";
 import { RouterLink } from "vue-router";
-const message = ref<string>('index message');
+
+
+// ダミーデータ
+type Summary = {
+    id: number;
+    title: string;
+};
+
+const articles = ref<Map<number, Summary>>(new Map());
+articles.value.set(1, { id: 1, title: "Laravel + Vue3やってみる" });
+articles.value.set(2, { id: 2, title: "TypeScript入門" });
+
 </script>
-  
+
+<style scoped>
+th {
+    border: black 1px solid;
+}
+
+td {
+    border: black 1px solid;
+}
+</style>
